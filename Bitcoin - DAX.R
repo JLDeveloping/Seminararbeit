b0c93176eb78eb8997d5b20886304721bb44e7b0
JLDeveloping <62248523+JLDeveloping@users.noreply.github.com>
2020-12-05 14:49:23 +0100

    Aufgeräumt und Renditefunktion hinzugefügt

--- Bitcoin - DAX.R
+++ Bitcoin - DAX.R
@@ -22,28 +22,24 @@ kurswahl = 6                     # Kurswahl: 1=Open, 2=High, 3=Low, 4=Close, 5=V
 DAX = getKurse("^GDAXI", startZeitraum, endzeitraum, kurswahl)
 BitcoinEUR = getKurse("BTC-EUR", startZeitraum, endzeitraum, kurswahl)
 
-
-##
-zeitachse = time(BitcoinEUR)   #Zeitachse des Objektes
-
-
-#Berechnung der Mittelwerte
-Bitcoinmittelwert=mean(BitcoinEURBeobachtungen[,kurswahl])
-DAXMittelwert = mean(DAXBeobachtungen[,kurswahl])
-#Standardabweichung
-BitcoinStandardabweichung = sd(BitcoinEURBeobachtungen[,kurswahl])
-DAXStandardabweichung = sd(DAXBeobachtungen[,kurswahl])
-
 #Jahreswerte
-Bfirstdays = do.call(rbind, lapply(split(BitcoinEURBeobachtungen, "years"), first))
-BlastDays  = do.call(rbind, lapply(split(BitcoinEURBeobachtungen, "years"), last))
+Bfirstdays = do.call(rbind, lapply(split(BitcoinEUR, "years"), first))
+BlastDays  = do.call(rbind, lapply(split(BitcoinEUR, "years"), last))
 
-Return.calculate(to.yearly(BitcoinEURBeobachtungen))
+getReturn("jährlich", "last", BitcoinEUR)
 
 VaR()
 
 
 #matrix = as.matrix(DAX)
+#zeitachse = time(BitcoinEUR)   #Zeitachse des Objektes
+
+#Berechnung der Mittelwerte
+#Bitcoinmittelwert=mean(BitcoinEUR)
+#DAXMittelwert = mean(DAX)
+#Standardabweichung
+#BitcoinStandardabweichung = sd(BitcoinEUR)
+#DAXStandardabweichung = sd(DAX)
 
 
 
@@ -52,7 +48,21 @@ VaR()
 #Funktionen
 #########
 
+#Liefert die Kursdaten des Kurses "symbols" von im Zeitraum start-ende
+#kurs gibt an, welche Art der Kursdaten verwendet werden soll
 getKurse = function(symbols, start, ende, kurs){
   tempKurs = getSymbols(Symbols = symbols, from = start, auto.assign = FALSE)[,kurswahl];
   tempKurs[paste0('/',ende)]
 }
+
+#Liefert den Return
+#periode gibt an ob jährlich, wöchentlich oder täglich
+#day gibt an, ob first=erster Tag pro Periode, last=letzter Tag pro Periode
+getReturn = function(periode, day, data){
+  if (periode == "jährlich") {
+    temp = do.call(rbind, lapply(split(data, "years"), last));
+  };
+  Return.calculate(temp)
+}
+
+
