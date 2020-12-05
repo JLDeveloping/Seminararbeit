ce4a299fa5836d2ab1758b1efa36ccea8887f022
JLDeveloping <62248523+JLDeveloping@users.noreply.github.com>
2020-12-05 14:54:30 +0100

    Funktionen ausgelagert

--- Bitcoin - DAX.R
+++ Bitcoin - DAX.R
@@ -7,6 +7,9 @@ install.packages("devtools")
 install.packages("PerformanceAnalytics")
 
 ##
+source("Funktionen.R")
+
+##
 library(quantmod)
 library(ggplot2)
 library(rugarch)
@@ -40,29 +43,4 @@ VaR()
 #Standardabweichung
 #BitcoinStandardabweichung = sd(BitcoinEUR)
 #DAXStandardabweichung = sd(DAX)
-
-
-
-
-#########
-#Funktionen
-#########
-
-#Liefert die Kursdaten des Kurses "symbols" von im Zeitraum start-ende
-#kurs gibt an, welche Art der Kursdaten verwendet werden soll
-getKurse = function(symbols, start, ende, kurs){
-  tempKurs = getSymbols(Symbols = symbols, from = start, auto.assign = FALSE)[,kurswahl];
-  tempKurs[paste0('/',ende)]
-}
-
-#Liefert den Return
-#periode gibt an ob jährlich, wöchentlich oder täglich
-#day gibt an, ob first=erster Tag pro Periode, last=letzter Tag pro Periode
-getReturn = function(periode, day, data){
-  if (periode == "jährlich") {
-    temp = do.call(rbind, lapply(split(data, "years"), last));
-  };
-  Return.calculate(temp)
-}
-
 
